0cdbdc8654bd9f335d3fda14dedfd37c487aebde
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2023-06-16 13:28:11 +0200

    fix missing dir issue

--- packages/subgraph/scripts/postInstall.ts
+++ packages/subgraph/scripts/postInstall.ts
@@ -3,28 +3,31 @@ import * as osx120 from 'osx-ethersV120';
 import path from 'path';
 
 // Extract the parameters from the arguments
-const params = ['PluginRepo', 'PluginSetupProcessor'];
+const params = [
+  osx120.PluginRepoFactory__factory,
+  osx120.PluginSetupProcessor__factory,
+];
 
-function generateABIFiles(params: string[]): void {
+function generateABIFiles(params: any[]): void {
   params.forEach(param => {
-    const artifact = (osx120 as any)[`${param}__factory`];
-
-    if (!artifact) {
-      throw new Error(`Artifact not found for: ${param}`);
-    }
-
-    const abiObject: any = artifact.abi;
+    const abiObject: any = param.abi;
+    const abiName: string = param.name.replace('__factory', '');
 
     // Write the ABI object to a JSON file
-    const fileName = `${param}.json`;
+    const fileName = `${abiName}.json`;
 
     // Construct the ABI directory path relative to the project root
     const abisDirectory = path.join(__dirname, '../abis/generated');
 
+    // Check if directory exists, if not create it
+    if (!fs.existsSync(abisDirectory)) {
+      fs.mkdirSync(abisDirectory, {recursive: true});
+    }
+
     const filePath = path.resolve(abisDirectory, fileName);
     fs.writeFileSync(filePath, JSON.stringify(abiObject));
 
-    console.log(`ABI file generated for '${param}', file: ${filePath}`);
+    console.log(`ABI file generated for '${abiName}', file: ${filePath}`);
   });
 }
 
