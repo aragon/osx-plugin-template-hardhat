8846a740c4495fad4b8852879f3c6ff773e9e620
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2023-11-07 16:48:09 +0100

    subgraph: update test constants and comments

--- packages/subgraph/tests/utils/constants.ts
+++ packages/subgraph/tests/utils/constants.ts
@@ -1,4 +1,8 @@
-// Random Address and Hex strings for testing purposes.
+// Constants for test scenarios:
+// ADDRESS_ZERO to ADDRESS_SIX: Dummy Ethereum addresses for various test cases.
+// DAO_ADDRESS: A placeholder address for a DAO instance.
+// CONTRACT_ADDRESS: A placeholder address for a contract instance.
+// PLUGIN_SETUP_ID: A mock identifier for a plugin setup in test simulations.
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000';
 export const ADDRESS_ONE = '0x0000000000000000000000000000000000000001';
 export const ADDRESS_TWO = '0x0000000000000000000000000000000000000002';
@@ -8,9 +12,5 @@ export const ADDRESS_FIVE = '0x0000000000000000000000000000000000000005';
 export const ADDRESS_SIX = '0x0000000000000000000000000000000000000006';
 export const DAO_ADDRESS = '0x00000000000000000000000000000000000000da';
 export const CONTRACT_ADDRESS = '0x00000000000000000000000000000000000000Ad';
-export const DAO_TOKEN_ADDRESS = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
-
 export const PLUGIN_SETUP_ID =
   '0xfb3fd2c4cd4e19944dd3f8437e67476240cd9e3efb2294ebd10c59c8f1d6817c';
-export const APPLIED_PLUGIN_SETUP_ID =
-  '0x00000000cd4e19944dd3f8437e67476240cd9e3efb2294ebd10c59c8f1d6817c';
