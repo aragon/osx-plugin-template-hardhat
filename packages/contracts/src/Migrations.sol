1cf28949da977a2517b1e3538943cffd72a418a5
Michael Heuer <michael.heuer@rwth-aachen.de>
2023-11-08 15:32:28 +0100

    docs: improve migration file comment

--- packages/contracts/src/Migrations.sol
+++ packages/contracts/src/Migrations.sol
@@ -2,9 +2,8 @@
 
 pragma solidity 0.8.17;
 
-// Importing files here will result in additional artifacts and TypeChain bindings being generated.
-// These might be older versions of your contracts or other contracts that you don't import in your code,
-// but that you which you might require for integration testing or deployment.
+// Additional, external solidity files can be imported here to generate artifacts and TypeChain bindings.
+// These might be older versions of your own contracts or third-party contracts that you don't import in your code, but require for integration testing or deployment.
 
 // solhint-disable no-unused-import
 import {DAOFactory} from "@aragon/osx/framework/dao/DAOFactory.sol";
