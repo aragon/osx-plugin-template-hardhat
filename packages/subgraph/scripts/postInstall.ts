eecd6303a83cf99d74cc57f476606d11cc0ce7f1
Jose Manuel Mariñas Bascoy <josemarinasbascoy@gmail.com>
2023-12-19 11:42:22 +0100

    remove alias import

--- packages/subgraph/scripts/postInstall.ts
+++ packages/subgraph/scripts/postInstall.ts
@@ -1,10 +1,10 @@
 // Import the osx-ethers module to access OSX contracts and generate their ABIs for any needed version.
-import * as osx from '@aragon/osx-ethers';
+import {PluginSetupProcessor__factory} from '@aragon/osx-ethers';
 import fs from 'fs';
 import path from 'path';
 
 // Add the contract factories to this array for the contracts you want to generate ABIs for.
-const contractFactories = [osx.PluginSetupProcessor__factory];
+const contractFactories = [PluginSetupProcessor__factory];
 
 function generateABIFiles(contractFactories: any[]): void {
   // Iterate through each contract factory passed.
