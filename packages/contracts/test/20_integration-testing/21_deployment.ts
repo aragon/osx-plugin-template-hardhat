60b316d6f97ef9012cda98f4877979952837a597
jordaniza <jordan.imran@predixa.ai>
2024-03-05 12:43:48 +0400

    revert: bring back integration test files for diffs

---
+++ packages/contracts/test/20_integration-testing/21_deployment.ts
@@ -0,0 +1,123 @@
+// import {METADATA, VERSION} from '../../plugin-settings';
+// import {getProductionNetworkName, findPluginRepo} from '../../utils/helpers';
+// import {
+//   getLatestNetworkDeployment,
+//   getNetworkNameByAlias,
+// } from '@aragon/osx-commons-configs';
+// import {
+//   DAO_PERMISSIONS,
+//   PERMISSION_MANAGER_FLAGS,
+//   PLUGIN_REPO_PERMISSIONS,
+//   UnsupportedNetworkError,
+//   toHex,
+//   uploadToIPFS,
+// } from '@aragon/osx-commons-sdk';
+// import {
+//   PluginRepo,
+//   PluginRepoRegistry,
+//   PluginRepoRegistry__factory,
+// } from '@aragon/osx-ethers';
+// import {loadFixture} from '@nomicfoundation/hardhat-network-helpers';
+// import {SignerWithAddress} from '@nomiclabs/hardhat-ethers/signers';
+// import {expect} from 'chai';
+// import env, {deployments, ethers} from 'hardhat';
+
+// const productionNetworkName = getProductionNetworkName(env);
+
+// describe(`Deployment on network '${productionNetworkName}'`, function () {
+//   it('creates the repo', async () => {
+//     const {pluginRepo, pluginRepoRegistry} = await loadFixture(fixture);
+
+//     expect(await pluginRepoRegistry.entries(pluginRepo.address)).to.be.true;
+//   });
+
+//   it('makes the deployer the repo maintainer', async () => {
+//     const {deployer, pluginRepo} = await loadFixture(fixture);
+
+//     expect(
+//       await pluginRepo.isGranted(
+//         pluginRepo.address,
+//         deployer.address,
+//         DAO_PERMISSIONS.ROOT_PERMISSION_ID,
+//         PERMISSION_MANAGER_FLAGS.NO_CONDITION
+//       )
+//     ).to.be.true;
+
+//     expect(
+//       await pluginRepo.isGranted(
+//         pluginRepo.address,
+//         deployer.address,
+//         PLUGIN_REPO_PERMISSIONS.UPGRADE_REPO_PERMISSION_ID,
+//         PERMISSION_MANAGER_FLAGS.NO_CONDITION
+//       )
+//     ).to.be.true;
+
+//     expect(
+//       await pluginRepo.isGranted(
+//         pluginRepo.address,
+//         deployer.address,
+//         PLUGIN_REPO_PERMISSIONS.MAINTAINER_PERMISSION_ID,
+//         PERMISSION_MANAGER_FLAGS.NO_CONDITION
+//       )
+//     ).to.be.true;
+//   });
+
+//   context('PluginSetup Publication', async () => {
+//     it('registers the setup', async () => {
+//       const {pluginRepo} = await loadFixture(fixture);
+
+//       await pluginRepo['getVersion((uint8,uint16))']({
+//         release: VERSION.release,
+//         build: VERSION.build,
+//       });
+
+//       const results = await pluginRepo['getVersion((uint8,uint16))']({
+//         release: VERSION.release,
+//         build: VERSION.build,
+//       });
+
+//       const buildMetadataURI = `ipfs://${await uploadToIPFS(
+//         JSON.stringify(METADATA.build, null, 2)
+//       )}`;
+
+//       expect(results.buildMetadata).to.equal(toHex(buildMetadataURI));
+//     });
+//   });
+// });
+
+// type FixtureResult = {
+//   deployer: SignerWithAddress;
+//   pluginRepo: PluginRepo;
+//   pluginRepoRegistry: PluginRepoRegistry;
+// };
+
+// async function fixture(): Promise<FixtureResult> {
+//   // Deploy all
+//   const tags = ['CreateRepo', 'NewVersion'];
+//   await deployments.fixture(tags);
+
+//   const [deployer] = await ethers.getSigners();
+
+//   // Plugin Repo
+//   const {pluginRepo, ensDomain} = await findPluginRepo(env);
+//   if (pluginRepo === null) {
+//     throw `PluginRepo '${ensDomain}' does not exist yet.`;
+//   }
+
+//   const network = getNetworkNameByAlias(productionNetworkName);
+//   if (network === null) {
+//     throw new UnsupportedNetworkError(productionNetworkName);
+//   }
+//   const networkDeployments = getLatestNetworkDeployment(network);
+//   if (networkDeployments === null) {
+//     throw `Deployments are not available on network ${network}.`;
+//   }
+
+//   // Plugin repo registry
+//   const pluginRepoRegistry = PluginRepoRegistry__factory.connect(
+//     networkDeployments.PluginRepoRegistryProxy.address,
+//     deployer
+//   );
+
+//   return {deployer, pluginRepo, pluginRepoRegistry};
+// }
---
+++ packages/contracts/test/20_integration-testing/test-helpers.ts
@@ -0,0 +1,214 @@
+import {DAOMock, IPlugin} from '../../typechain';
+import {hashHelpers} from '../../utils/helpers';
+import {
+  DAO_PERMISSIONS,
+  PLUGIN_SETUP_PROCESSOR_PERMISSIONS,
+  findEvent,
+} from '@aragon/osx-commons-sdk';
+import {
+  PluginSetupProcessorEvents,
+  PluginSetupProcessorStructs,
+  PluginSetupProcessor,
+  DAOStructs,
+} from '@aragon/osx-ethers';
+import {SignerWithAddress} from '@nomiclabs/hardhat-ethers/signers';
+import {expect} from 'chai';
+import {ContractTransaction} from 'ethers';
+
+export async function installPLugin(
+  signer: SignerWithAddress,
+  psp: PluginSetupProcessor,
+  dao: DAOMock,
+  pluginSetupRef: PluginSetupProcessorStructs.PluginSetupRefStruct,
+  data: string
+): Promise<{
+  prepareTx: ContractTransaction;
+  applyTx: ContractTransaction;
+  preparedEvent: PluginSetupProcessorEvents.InstallationPreparedEvent;
+  appliedEvent: PluginSetupProcessorEvents.InstallationAppliedEvent;
+}> {
+  const prepareTx = await psp.connect(signer).prepareInstallation(dao.address, {
+    pluginSetupRef: pluginSetupRef,
+    data: data,
+  });
+
+  const preparedEvent =
+    await findEvent<PluginSetupProcessorEvents.InstallationPreparedEvent>(
+      prepareTx,
+      psp.interface.getEvent('InstallationPrepared').name
+    );
+
+  const plugin = preparedEvent.args.plugin;
+  const preparedPermissions = preparedEvent.args.preparedSetupData.permissions;
+
+  await checkPermissions(
+    preparedPermissions,
+    dao,
+    psp,
+    signer,
+    PLUGIN_SETUP_PROCESSOR_PERMISSIONS.APPLY_INSTALLATION_PERMISSION_ID
+  );
+
+  const applyTx = await psp.connect(signer).applyInstallation(dao.address, {
+    pluginSetupRef: pluginSetupRef,
+    plugin: plugin,
+    permissions: preparedPermissions,
+    helpersHash: hashHelpers(preparedEvent.args.preparedSetupData.helpers),
+  });
+
+  const appliedEvent =
+    await findEvent<PluginSetupProcessorEvents.InstallationAppliedEvent>(
+      applyTx,
+      psp.interface.getEvent('InstallationApplied').name
+    );
+
+  return {prepareTx, applyTx, preparedEvent, appliedEvent};
+}
+
+export async function uninstallPLugin(
+  signer: SignerWithAddress,
+  psp: PluginSetupProcessor,
+  dao: DAOMock,
+  plugin: IPlugin,
+  pluginSetupRef: PluginSetupProcessorStructs.PluginSetupRefStruct,
+  data: string,
+  currentHelpers: string[]
+): Promise<{
+  prepareTx: ContractTransaction;
+  applyTx: ContractTransaction;
+  preparedEvent: PluginSetupProcessorEvents.UninstallationPreparedEvent;
+  appliedEvent: PluginSetupProcessorEvents.UninstallationAppliedEvent;
+}> {
+  const prepareTx = await psp
+    .connect(signer)
+    .prepareUninstallation(dao.address, {
+      pluginSetupRef: pluginSetupRef,
+      setupPayload: {
+        plugin: plugin.address,
+        currentHelpers: currentHelpers,
+        data: data,
+      },
+    });
+
+  const preparedEvent =
+    await findEvent<PluginSetupProcessorEvents.UninstallationPreparedEvent>(
+      prepareTx,
+      psp.interface.getEvent('UninstallationPrepared').name
+    );
+
+  const preparedPermissions = preparedEvent.args.permissions;
+
+  await checkPermissions(
+    preparedPermissions,
+    dao,
+    psp,
+    signer,
+    PLUGIN_SETUP_PROCESSOR_PERMISSIONS.APPLY_UNINSTALLATION_PERMISSION_ID
+  );
+
+  const applyTx = await psp.connect(signer).applyUninstallation(dao.address, {
+    plugin: plugin.address,
+    pluginSetupRef: pluginSetupRef,
+    permissions: preparedPermissions,
+  });
+
+  const appliedEvent =
+    await findEvent<PluginSetupProcessorEvents.UninstallationAppliedEvent>(
+      applyTx,
+      psp.interface.getEvent('UninstallationApplied').name
+    );
+
+  return {prepareTx, applyTx, preparedEvent, appliedEvent};
+}
+export async function updatePlugin(
+  signer: SignerWithAddress,
+  psp: PluginSetupProcessor,
+  dao: DAOMock,
+  plugin: IPlugin,
+  currentHelpers: string[],
+  pluginSetupRefCurrent: PluginSetupProcessorStructs.PluginSetupRefStruct,
+  pluginSetupRefUpdate: PluginSetupProcessorStructs.PluginSetupRefStruct,
+  data: string
+): Promise<{
+  prepareTx: ContractTransaction;
+  applyTx: ContractTransaction;
+  preparedEvent: PluginSetupProcessorEvents.UpdatePreparedEvent;
+  appliedEvent: PluginSetupProcessorEvents.UpdateAppliedEvent;
+}> {
+  expect(pluginSetupRefCurrent.pluginSetupRepo).to.equal(
+    pluginSetupRefUpdate.pluginSetupRepo
+  );
+
+  const prepareTx = await psp.connect(signer).prepareUpdate(dao.address, {
+    currentVersionTag: pluginSetupRefCurrent.versionTag,
+    newVersionTag: pluginSetupRefUpdate.versionTag,
+    pluginSetupRepo: pluginSetupRefUpdate.pluginSetupRepo,
+    setupPayload: {
+      plugin: plugin.address,
+      currentHelpers: currentHelpers,
+      data: data,
+    },
+  });
+  const preparedEvent =
+    await findEvent<PluginSetupProcessorEvents.UpdatePreparedEvent>(
+      prepareTx,
+      psp.interface.getEvent('UpdatePrepared').name
+    );
+
+  const preparedPermissions = preparedEvent.args.preparedSetupData.permissions;
+
+  await checkPermissions(
+    preparedPermissions,
+    dao,
+    psp,
+    signer,
+    PLUGIN_SETUP_PROCESSOR_PERMISSIONS.APPLY_UPDATE_PERMISSION_ID
+  );
+
+  const applyTx = await psp.connect(signer).applyUpdate(dao.address, {
+    plugin: plugin.address,
+    pluginSetupRef: pluginSetupRefUpdate,
+    initData: preparedEvent.args.initData,
+    permissions: preparedPermissions,
+    helpersHash: hashHelpers(preparedEvent.args.preparedSetupData.helpers),
+  });
+  const appliedEvent =
+    await findEvent<PluginSetupProcessorEvents.UpdateAppliedEvent>(
+      applyTx,
+      psp.interface.getEvent('UpdateApplied').name
+    );
+
+  return {prepareTx, applyTx, preparedEvent, appliedEvent};
+}
+
+async function checkPermissions(
+  preparedPermissions: DAOStructs.MultiTargetPermissionStruct[],
+  dao: DAOMock,
+  psp: PluginSetupProcessor,
+  signer: SignerWithAddress,
+  applyPermissionId: string
+) {
+  if (preparedPermissions.length !== 0) {
+    if (
+      !(await dao.hasPermission(
+        dao.address,
+        psp.address,
+        DAO_PERMISSIONS.ROOT_PERMISSION_ID,
+        []
+      ))
+    ) {
+      throw `The 'PluginSetupProcessor' does not have 'ROOT_PERMISSION_ID' on the DAO and thus cannot process the list of permissions requested by the plugin setup.`;
+    }
+  }
+  if (
+    signer.address !== dao.address &&
+    !(await dao.hasPermission(
+      psp.address,
+      signer.address,
+      applyPermissionId,
+      []
+    ))
+  ) {
+    throw `The used signer does not have the permission with ID '${applyPermissionId}' granted and thus cannot apply the setup`;
+  }
+}
