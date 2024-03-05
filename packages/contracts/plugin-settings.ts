8c5f7b43f1435b7c0d1c0307792ff1a764b87ff0
Claudia <claudiabarcelovaldes40@gmail.com>
2024-03-05 10:57:57 +0100

    feat: rename the plugin subdomain

--- packages/contracts/plugin-settings.ts
+++ packages/contracts/plugin-settings.ts
@@ -4,7 +4,7 @@ import {VersionTag} from '@aragon/osx-commons-sdk';
 
 export const PLUGIN_CONTRACT_NAME = 'MyPlugin'; // This must match the filename `packages/contracts/src/MyPlugin.sol` and the contract name `MyPlugin` within.
 export const PLUGIN_SETUP_CONTRACT_NAME = 'MyPluginSetup'; // This must match the filename `packages/contracts/src/MyPluginSetup.sol` and the contract name `MyPluginSetup` within.
-export const PLUGIN_REPO_ENS_SUBDOMAIN_NAME = 'my'; // This will result in the ENS domain name 'my.plugin.dao.eth'
+export const PLUGIN_REPO_ENS_SUBDOMAIN_NAME = 'myP'; // This will result in the ENS domain name 'my.plugin.dao.eth'
 
 export const VERSION: VersionTag = {
   release: 1, // Increment this number ONLY if breaking/incompatible changes were made. Updates between releases are NOT possible.
