eccf75c7da8b8c2dfe47cd1ff7ccde3359efc099
jordaniza <jordan.imran@predixa.ai>
2024-03-14 18:16:31 +0400

    tests: default to polygon PoS as build test 1.0.0 does not exist on sepolia

--- packages/contracts/utils/helpers.ts
+++ packages/contracts/utils/helpers.ts
@@ -27,7 +27,11 @@ import {ethers} from 'hardhat';
 import {HardhatRuntimeEnvironment} from 'hardhat/types';
 
 export function isLocal(hre: HardhatRuntimeEnvironment): boolean {
-  return hre.network.name === 'localhost' || hre.network.name === 'hardhat';
+  return (
+    hre.network.name === 'localhost' ||
+    hre.network.name === 'hardhat' ||
+    hre.network.name === 'coverage'
+  );
 }
 
 export function getProductionNetworkName(
@@ -39,9 +43,9 @@ export function getProductionNetworkName(
       productionNetworkName = process.env.NETWORK_NAME;
     } else {
       console.log(
-        `No network has been provided in the '.env' file. Defaulting to '${SupportedNetworks.SEPOLIA}' as the production network.`
+        `No network has been provided in the '.env' file. Defaulting to '${SupportedNetworks.POLYGON}' as the production network.`
       );
-      productionNetworkName = SupportedNetworks.SEPOLIA;
+      productionNetworkName = SupportedNetworks.POLYGON;
     }
   } else {
     productionNetworkName = hre.network.name;
